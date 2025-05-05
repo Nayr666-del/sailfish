339033dd84949640cb96afd10c6e99203e0f94db
David O'Neill <davidoneill@Davids-MacBook-Pro.local>
2025-05-05 14:03:19 +0200

    Correct preband computations for GPUs

--- sailfish/solvers/cbdgam_2d.py
+++ sailfish/solvers/cbdgam_2d.py
@@ -408,7 +408,7 @@ class Solver(SolverBase):
             return Interpolated_Optical, Interpolated_Infared
         
         except IndexError as e:
-            if RescaledTemp > self.Precompute_Band_Luminosities[0][-1]:
+            if RescaledTemp.any() > self.Precompute_Band_Luminosities[0][-1]:
                 raise IndexError("Interpolated temperature range needs to be higher in cbdgam_2d.py. Current value is logT = %g"%(np.log10(self.Precompute_Band_Luminosities[0][-1])))
             else:
                 raise IndexError("Different Error")
