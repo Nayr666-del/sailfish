b191d498e39a3ba1760897a54c4a1327afc58be4
David O'Neill <davidoneill@Davids-MacBook-Pro.local>
2025-05-05 14:49:10 +0200

    Correct preband computations for GPUs

--- sailfish/solvers/cbdgam_2d.py
+++ sailfish/solvers/cbdgam_2d.py
@@ -412,17 +412,10 @@ class Solver(SolverBase):
         except IndexError as e:
             if RescaledTemp.any() > self.Precompute_Band_Luminosities[0][-1]:
                 raise IndexError("Interpolated temperature range limit needs to be higher in cbdgam_2d.py. Current value is logT = %g"%(np.log10(self.Precompute_Band_Luminosities[0][-1])))
-            else:
-                print(np.min(Sigma))
-                print(Sigma.any() == 0.0)
-                print(np.min(Sigma) == 0.0)
-                raise ValueError("Lightcurve reductions failed at time t = %g due to nan Sigma"%(1))
-
-            #elif Sigma.any() == 0.:
-            #    print(dir(patch))
-            #    raise ValueError("Lightcurve reductions failed at time t = %g due to nan Sigma"%(1))
-            #elif RescaledTemp.any() < self.Precompute_Band_Luminosities[0][0]:
-            #    raise IndexError("Interpolated temperature range limit needs to be lower in cbdgam_2d.py. Current value is logT = %g"%(np.log10(self.Precompute_Band_Luminosities[0][0])))
+            elif np.min(Sigma) == 0.0:
+                raise ValueError("Lightcurve reductions failed at time t = %g due to Sigma == 0.0 in cell"%(1))
+            elif RescaledTemp.any() < self.Precompute_Band_Luminosities[0][0]:
+                raise IndexError("Interpolated temperature range limit needs to be lower in cbdgam_2d.py. Current value is logT = %g"%(np.log10(self.Precompute_Band_Luminosities[0][0])))
 
             
 
